c7da899d6cc99703312bb62973ca4563d429dd69
ximenagomeez <88910454+ximenagomeez@users.noreply.github.com>
2021-08-20 21:46:49 -0500

    Add files via upload

---
+++ PROYECTO.py
@@ -0,0 +1,21 @@
+"""
+Proyecto Calculadora básica
+
+1.Diseñar página
+2.agregar botones
+3.Input a operador
+4.funciones básicas, sumar restar, multiplicar, dividir
+5.posibles resultados
+6. print resultados
+7.loop
+
+"""
+#DISEÑO PÁGINA-----------------------------------------------------------
+from tkinter import *
+window=Tk()
+window.title("CALCULADORA BÁSICA")
+window.config(bg="pink")
+
+
+
+
