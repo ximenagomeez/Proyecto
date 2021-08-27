9625a98d1cadbaf1c73658f7fa0e4cb59fbef052
ximenagomeez <88910454+ximenagomeez@users.noreply.github.com>
2021-08-27 09:38:44 -0500

    Update PROYECTO.py

--- PROYECTO.py
+++ PROYECTO.py
@@ -16,6 +16,72 @@ window=Tk()
 window.title("CALCULADORA BÁSICA")
 window.config(bg="pink")
 
+#ACTIONCODE----------------------------------
 
+#NIVEL 1
+
+#LABEL0.1--------------------------------------
+Label0=Label(window, text="              ")
+Label0.grid(column=2, row=1, padx=15, pady=15)
+
+
+#NIVEL 2
+
+#LABEL1--------------------------------------
+Label1=Label(window, text=" 1 ")
+Label1.grid(column=1, row=2, padx=15, pady=15)
+
+###3Button1=Button(command=action)
+
+#LABEL2--------------------------------------
+Label1=Label(window, text=" 2 ")
+Label1.grid(column=2, row=2, padx=15, pady=15)
+
+#LABEL3--------------------------------------
+Label1=Label(window, text=" 3 ")
+Label1.grid(column=3, row=2, padx=15, pady=15)
+
+#LABEL4--------------------------------------
+Label1=Label(window, text=" 4 ")
+Label1.grid(column=4, row=2, padx=15, pady=15)
+
+#NIVEL 3
+
+#LABEL5--------------------------------------
+Label1=Label(window, text=" 5 ")
+Label1.grid(column=1, row=3, padx=15, pady=15)
+
+#LABEL6--------------------------------------
+Label1=Label(window, text=" 6 ")
+Label1.grid(column=2, row=3, padx=15, pady=15)
+
+#LABEL7--------------------------------------
+Label1=Label(window, text=" 7 ")
+Label1.grid(column=3, row=3, padx=15, pady=15)
+
+#LABEL8--------------------------------------
+Label1=Label(window, text=" 8 ")
+Label1.grid(column=4, row=3, padx=15, pady=15)
+
+#NIVEL 4
+
+#LABEL9--------------------------------------
+Label1=Label(window, text=" 9 ")
+Label1.grid(column=1, row=4, padx=15, pady=15)
+
+#LABEL00--------------------------------------
+Label1=Label(window, text=" 0 ")
+Label1.grid(column=2, row=4, padx=15, pady=15)
+
+#LABEL7--------------------------------------
+Label1=Label(window, text=" <- ")
+Label1.grid(column=3, row=4, padx=15, pady=15)
+
+#LABEL8--------------------------------------
+Label1=Label(window, text=" ENTER ")
+Label1.grid(column=4, row=4, padx=15, pady=15)
+
+
+window.mainloop()
 
 
